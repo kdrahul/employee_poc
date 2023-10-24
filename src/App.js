2ba2fa35fb52c203c1ab38e8751134dcfa9cf86a
Rahul Kulkarni <kdrahul@github.com>
2023-10-24 15:25:05 +0530

    Added form validation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { createContext } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import EmployeeForm from "./EmployeeForm";
 
 const App = () => {
-    
     return (
         <div>
             <h1>Employee Details</h1>
--- src/EmployeeForm.jsx
+++ src/EmployeeForm.jsx
@@ -1,51 +1,126 @@
-import React, { useState } from 'react';
+import React, {  useState } from 'react';
 
 const EmployeeForm = () => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [dob, setDob] = useState('');
-    const [address, setAddress] = useState('');
 
-    // TODO Have separate useEffect for each items to render error message only for that
-    // TODO Validate whole form onSubmit
-    // TODO Append the form data to a global context after its validated
+    const [values, setValues] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        dob: '',
+        gender: '',
+        address: '',
+    });
+
+    const [errors, setErrors] = useState({});
+    const [valid, setValid] = useState({});
+
+    const validateForm = () => {
+        const err = {}
+        const val = {}
+
+        if(values.firstName === '') {
+            val.firstName = 'is-invalid';
+            err.firstName = 'Please provide your first name!';
+        } else {
+            val.firstName = 'is-valid';
+        }
+        if(values.lastName === '') {
+            val.lastName = 'is-invalid';
+            err.lastName = 'Please provide your last name!';
+        } else {
+            val.lastName = 'is-valid';
+        }
+        if(values.email === '') {
+            val.email = 'is-invalid';
+            err.email = 'How do we contact you?';
+        } else {
+            val.email = 'is-valid';
+        }
+        if(values.gender === '') {
+            val.gender = 'is-invalid';
+            err.gender = 'How do we address you?';
+        } else {
+            val.gender = 'is-valid';
+        }
+        if(values.address === '') {
+            val.address = 'is-invalid';
+            err.address = "Tell us where you're from";
+        } else {
+            val.address = 'is-valid';
+        }
+        if(values.dob === '') {
+            val.dob = 'is-invalid';
+            err.dob = 'Tell us when you were born!';
+        } else {
+            val.dob = 'is-valid';
+        }
+
+        setErrors({...err});
+        setValid({...val});
+
+        console.table(errors);
+        console.table(valid);
+
+        return false;
+
+    }
+
+    const onBlurHandler = (e) => {
+        setValues(() => ({
+            ...values,
+            [e.target.name]: e.target.value
+        }))
+        // console.table(values);
+    }
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        const isValid = validateForm();
+        console.log(isValid);
+    }
 
     return (
-        <form className='container'>
+        <form onSubmit={onSubmitHandler} className='container'>
             <div className='form-group mb-2'>
-                <label for="firstNameInput">First Name</label>
-                <input type='text' className='form-control' id='firstNameInputId' placeholder='First Name' />
+                <label htmlFor="firstNameInput">First Name</label>
+                <input type='text' name='firstName' className={'form-control ' + valid.firstName} id='firstNameInputId' placeholder='First Name' onBlur={onBlurHandler} aria-describedby='firstnameFeedback'  />
+                <span id='firstnameFeedback' className='invalid-feedback'>{errors.firstName}</span>
             </div>
             <div className='form-group mb-2'>
-                <label for="lastNameInput">Last Name</label>
-                <input type='text' className='form-control' id='lastNameInputId' placeholder='Last Name' />
+                <label htmlFor="lastNameInput">Last Name</label>
+                <input type='text' name='lastName' className={'form-control ' + valid.lastName} id='lastNameInputId' placeholder='Last Name' onBlur={onBlurHandler} aria-describedby='lastNameFeedback'  />
+                <span id="lastNameFeedback" className='invalid-feedback'>{errors.lastName}</span>
             </div>
             <div className='form-group mb-2'>
-                <label for="emailInput">Email</label>
-                <input type='email' className='form-control' id='emailInputId' placeholder='username@mail.com' />
+                <label htmlFor="emailInput">Email</label>
+                <input type='email' name='email' className={'form-control ' + valid.email} id='emailInputId' placeholder='username@mail.com' onBlur={onBlurHandler} aria-describedby='emailFeedback'  />
+                <span id="emailFeedback" className='invalid-feedback'>{errors.email}</span>
             </div>
             <div className='form-group mb-2'>
-                <label for="dobInput">Date of Birth</label>
-                <input type='date' className='form-control' id='dobInputId' placeholder='01/01/1970' />
+                <label htmlFor="dobInput">Date of Birth</label>
+                <input type='date' name='dob' className={'form-control ' + valid.dob} id='dobInputId' placeholder='01/01/1970' onBlur={onBlurHandler} aria-describedby='dobFeedback'  />
+                <span id="dobFeedback" className='invalid-feedback'>{errors.dob}</span>
             </div>
             <div className='form-group mb-2'>
-                <label for="genderInput">Gender</label>
-                <select className='form-control' id='genderInputId'>
-                    <option>Prefer not to say</option>
-                    <option>Male</option>
-                    <option>Female</option>
-                    <option>Non-binary</option>
+                <label htmlFor="genderInput">Gender</label>
+                <select name='gender' className={'form-control ' + valid.gender} id='genderInputId' onBlur={onBlurHandler} aria-describedby='genderFeedback'  >
+                    <option value="">Select an option...</option>
+                    <option value="male">Mr. (Male)</option>
+                    <option value="female">Ms./Mrs. (Female)</option>
+                    <option value="others">Others</option>
                 </select>
+                <span id="genderFeedback" className='invalid-feedback'>{errors.gender}</span>
             </div>
             <div className='form-group mb-2'>
-                <label for="addressInput">Address</label>
-                <input type='text' className='form-control' id='addressInputId' placeholder='' />
+                <label htmlFor="addressInput">Address</label>
+                <input type='text' name='address' className={'form-control ' + valid.address} id='addressInputId' placeholder='Bengaluru,India' onBlur={onBlurHandler} aria-describedby='addressFeedback'   />
+                <span id="addressFeedback" className='invalid-feedback'>{errors.address}</span>
             </div>
 
 
             <div className='form-group mb-2'>
-                <button type='submit' className='btn btn-primary'>Submit</button>
+                <button type='submit' className='btn btn-primary' onClick={(e) => {console.log(e)}}>Submit</button>
             </div>
 
         </form>
